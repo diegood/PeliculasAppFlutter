e80ab4db2790dc31f8be19487611e2c2c4fcf175
Diego Gaston Arias Galeano <diegood@gmail.com>
2020-05-16 10:12:09 -0300

    voy a cambiar de FutereBuilder a StreamBuilder

---
+++ lib/src/models/pelicula_model.dart
@@ -0,0 +1,68 @@
+class Peliculas{
+  List<Pelicula> items = new List();
+  Peliculas();
+  Peliculas.fromJsonList( List<dynamic> jsonList){
+    if(jsonList == null) return;
+    for (var item in jsonList) {
+      items.add(Pelicula.formJsonMap(item));
+    }
+  }
+}
+
+class Pelicula {
+  double popularity;
+  int id;
+  bool video;
+  int voteCount;
+  double voteAverage;
+  String title;
+  String releaseDate;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String backdropPath;
+  bool adult;
+  String overview;
+  String posterPath;
+
+  Pelicula({
+    this.popularity,
+    this.id,
+    this.video,
+    this.voteCount,
+    this.voteAverage,
+    this.title,
+    this.releaseDate,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.backdropPath,
+    this.adult,
+    this.overview,
+    this.posterPath,
+  });
+
+  Pelicula.formJsonMap(Map<String, dynamic> json){
+    popularity = json['popularity'] / 1;
+    id = json['id'];
+    video = json['video'];
+    voteCount = json['vote_count'];
+    voteAverage = json['vote_average'] / 1;
+    title = json['title'];
+    releaseDate = json['release_date'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    genreIds = json['genre_ids'].cast<int>();
+    backdropPath = json['backdrop_path'];
+    adult = json['adult'];
+    overview = json['overview'];
+    posterPath = json['poster_path'];
+  }
+
+
+  getPosterImg(){
+    if(posterPath == null){return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcST3DnJqQp8xHyQIOv9rk9jp_6cAJyOG1YxjOYb_81C1sh6Hlme&usqp=CAU';}
+    return 'https://image.tmdb.org/t/p/w500/$posterPath';
+  }
+
+}
---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+
+class PeliculasProvider {
+  String _apikey = '656d98c5616a8de4b522063fa5feb2ee';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-AR';
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apikey, 'language': _language});
+
+    return await _procesarRespuesta(url);
+
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apikey, 'language': _language});
+
+    return await _procesarRespuesta(url);
+
+  }
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final peliculas = Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+}
